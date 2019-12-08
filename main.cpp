ba1217b80a2439932e3caca919fffd9155978737
Alexander Tyuryuchkin <tyuryuchkin@gmail.com>
2019-12-08 11:16:35 +0300

    ref Отключение окна консоли для windows

--- main.cpp
+++ main.cpp
@@ -1,8 +1,14 @@
 #include <QCoreApplication>
 #include "server.h"
+#if defined(WIN32) || defined(_WIN32)
+    #include <windows.h>
+#endif
 
 int main(int argc, char *argv[])
 {
+#if defined(WIN32) || defined(_WIN32)
+    ShowWindow(FindWindowA("ConsoleWindowClass", NULL), false);
+#endif
     QCoreApplication a(argc, argv);
     Server server;
     server.start();
